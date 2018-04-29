bf44336da0acff5bb1d13c6154a9d008063e6645
Jonathan Nguyen <jonguyen@quizor1.cs.pdx.edu>
2018-04-28 17:49:25 -0700

    a

--- Lab4/LLL/list.h
+++ Lab4/LLL/list.h
@@ -23,8 +23,48 @@ class list
     /* *****************YOUR TURN! ******************************** */
     //Write your function prototype here:
     
+    int display_except()
+    {
+       return display_except(head); 
+    }
 
-   private:		//notice there is both a head and a tail!
-      node * head;
-      node * tail;
-};
+
+    int num_times(int match)
+    {
+       
+        return num_times(head, match); 
+    }
+    private:		//notice there is both a head and a tail!
+
+    int num_times(node * head, int match)
+    {
+        if(!head)
+            return 0;
+
+       if(head->data != match)
+            return num_times(head->next, match);
+       else
+            return num_times(head->next, match)+ 1;
+    }
+
+          
+    int display_except(node * head)
+    {   
+        if(!head)
+            return 0;
+        return display_except(head->next);
+        
+        if((head == this->head) || (head->next == NULL))
+        {
+           return + head->data;
+        }
+     
+    }
+    
+    
+    node * head;
+    node * tail;
+    };
+
+
+
--- Lab4/LLL/main.cpp
+++ Lab4/LLL/main.cpp
@@ -5,11 +5,20 @@ int main()
     list object;
     object.build();    //builds a LLL
     object.display();  //displays the LLL
-
-    //PLEASE PUT YOUR CODE HERE to call the function assigned
-
-
     
+    //PLEASE PUT YOUR CODE HERE to call the function assigned
+    
+//
+//    int val;
+//    int response;
+//    cout << "Val to search : "; 
+//    cin >> response;
+//    val = object.num_times(response);
+//    cout << val;
+//  
+    int val;
+    val = object.display_except();
+    cout << val;  
 
     object.display();  //displays the LLL again!
    
